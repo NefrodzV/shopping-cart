90f6b07ab01671552a370beccb3e9dfec07e1658
Neftaly <neftalyrv23@gmail.com>
2023-11-06 18:16:56 -0400

    Documented the params

--- src/data/models/ShopProduct.js
+++ src/data/models/ShopProduct.js
@@ -1,12 +1,21 @@
-// Represents the shop product in the shop section
+// Represents the shop product in the shop section given by the service
 export default class ShopProduct {
-  constructor(id, title, price, description, category, imgUrl, rating) {
+  /**
+   * @param {number} id Identifier of the product
+   * @param {string} title Name of the product
+   * @param {number} price Price of the product
+   * @param {string} description Brief description of the product
+   * @param {string} category What category it belongs to
+   * @param {string} url Image url
+   * @param {number} rating Rating given by users
+   */
+  constructor(id, title, price, description, category, url, rating) {
     this.id = id
     this.title = title
     this.price = price
     this.description = description
     this.category = category
-    this.imgUrl = imgUrl
+    this.imgUrl = url
     this.rating = rating
   }
 }
