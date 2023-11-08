fdc3f77ecd98d8e1bfea3fabbf5876e9656f8a40
Neftaly <neftalyrv23@gmail.com>
2023-11-08 15:51:37 -0400

    Change to function component

--- src/components/Tag.jsx
+++ src/components/Tag.jsx
@@ -1,14 +1,5 @@
 import React from 'react'
-export default class Tag extends React.Component {
-  constructor() {
-    super()
-  }
-
-  render() {
-    return (
-      <div>
-        <p>50% Off</p>
-      </div>
-    )
-  }
+export default function Tag() {
+  // Will be displayed in items that have are on discount
+  return <>My tag</>
 }
