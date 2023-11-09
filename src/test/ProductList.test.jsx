209c0f551452146591a71bac9f2843f9d29c430b
Neftaly <neftalyrv23@gmail.com>
2023-11-09 15:04:02 -0400

    Simple test for product list

---
+++ src/test/ProductList.test.jsx
@@ -0,0 +1,30 @@
+import { describe, it, expect } from 'vitest'
+import { screen, render } from '@testing-library/react'
+import Navigation from '../components/Navigation'
+import { BrowserRouter } from 'react-router-dom'
+import ProductList from '../components/ProductList'
+
+describe('Product List', () => {
+  it('Render a list appropiately', () => {
+    const productsMock = [
+      {
+        id: 1,
+        title: 'My first product'
+      },
+      {
+        id: 2,
+        title: 'My second title'
+      }
+    ]
+
+    render(<ProductList products={productsMock} />, { wrapper: BrowserRouter })
+    const productsList = screen.getByRole('list')
+
+    expect(productsList).not.toBe(null)
+    expect(productsList).toBeInTheDocument()
+    expect(productsList.textContent).toContain('My first product')
+    expect(productsList.textContent).toContain('My second title')
+
+    // expect(productsList).toContainElement(productItem)
+  })
+})
