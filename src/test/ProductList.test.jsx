4f808148d7a2ef8d2df047ff1b8df1ae67330ed2
Neftaly <neftalyrv23@gmail.com>
2023-11-10 11:14:40 -0400

    Fix data change in test

--- src/test/ProductList.test.jsx
+++ src/test/ProductList.test.jsx
@@ -3,18 +3,29 @@ import { screen, render } from '@testing-library/react'
 import Navigation from '../components/Navigation'
 import { BrowserRouter } from 'react-router-dom'
 import ProductList from '../components/ProductList'
+import ProductData from '../data/models/Product'
 
 describe('Product List', () => {
   it('Render a list appropiately', () => {
     const productsMock = [
-      {
-        id: 1,
-        title: 'My first product'
-      },
-      {
-        id: 2,
-        title: 'My second title'
-      }
+      new ProductData(
+        1,
+        'My first product',
+        25,
+        'Best product of its quality',
+        'mens wear',
+        'firstproduct.png',
+        4.5
+      ),
+      new ProductData(
+        1,
+        'My second product',
+        20,
+        'Best product of its quality',
+        'mens wear',
+        'firstproduct.png',
+        4.1
+      )
     ]
     render(<ProductList products={productsMock} />, { wrapper: BrowserRouter })
     const productsList = screen.getByRole('list')
@@ -22,8 +33,6 @@ describe('Product List', () => {
     expect(productsList).not.toBe(null)
     expect(productsList).toBeInTheDocument()
     expect(productsList.textContent).toContain('My first product')
-    expect(productsList.textContent).toContain('My second title')
-
-    // expect(productsList).toContainElement(productItem)
+    expect(productsList.textContent).toContain('My second product')
   })
 })
