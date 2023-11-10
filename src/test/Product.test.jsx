fae0c7825c04ce5a3f5f6e503855339b2864adcb
Neftaly <neftalyrv23@gmail.com>
2023-11-10 10:39:15 -0400

    Test that product model renders in component

--- src/test/Product.test.jsx
+++ src/test/Product.test.jsx
@@ -1,9 +1,40 @@
 import { render, screen } from '@testing-library/react'
 import { describe, expect, vi } from 'vitest'
 import Product from '../components/Product'
+import ProductData from '../data/models/Product'
 import userEvent from '@testing-library/user-event'
 
 describe('Product', () => {
+  it('Should render the product data the correct way', () => {
+    const productData = new ProductData(
+      1,
+      'My first product',
+      25,
+      'Best product of its quality',
+      'mens wear',
+      'firstproduct.png',
+      4.5
+    )
+
+    render(
+      <Product
+        title={productData.getTitle}
+        url={productData.getUrl}
+        description={productData.getDescription}
+        price={productData.getPrice}
+      />
+    )
+
+    const product = screen.getByRole('listitem')
+    const productImage = screen.getByAltText(productData.description)
+    expect(product).toBeInTheDocument()
+    expect(product.textContent).toContain(productData.getTitle)
+    expect(product.textContent).toContain(productData.getPrice)
+
+    expect(productImage).toBeInTheDocument()
+    console.log(productImage)
+    expect(productImage).toHaveAttribute('src', productData.getUrl)
+  })
   it('Click listener gets called', async () => {
     const listenerMock = vi.fn() // Mocking a function
     render(<Product title="My product" listener={listenerMock} />)
