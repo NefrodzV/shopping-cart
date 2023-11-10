2a6d97b977a475420fcc1feb7aae9854e0647d19
Neftaly <neftalyrv23@gmail.com>
2023-11-10 11:15:19 -0400

    Update props and list creation

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,6 +1,25 @@
 import React from 'react'
+import Product from './Product'
 
 // Represents the item list
-export default function ProductList() {
-  return <div>My item List</div>
+export default function ProductList({ products, productListener }) {
+  const createProduct = (product) => {
+    return (
+      <Product
+        key={product.getId}
+        title={product.getTitle}
+        price={product.getPrice}
+        description={product.getDescription}
+        url={product.getUrl}
+        listener={productListener}
+      />
+    )
+  }
+
+  return (
+    <div>
+      <h2>Products</h2>
+      <ul>{products.map((product) => createProduct(product))}</ul>
+    </div>
+  )
 }
