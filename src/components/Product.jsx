06ce6ed15566def8892b2600f62e02442260ad43
Neftaly <neftalyrv23@gmail.com>
2023-11-10 11:10:43 -0400

    Product component updated with class and props

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 // Should represent the dom element item
-export default function Product() {
+export default function Product({ title, price, description, url, listener }) {
   return (
-    <div>
-      <img src="" alt="" />
-      <p></p>
-    </div>
+    <li className="product-card" onClick={listener}>
+      <p className="product-title">{title}</p>
+      <img className="product-image" src={url} alt={description} />
+      <p className="product-description">{price}</p>
+    </li>
   )
 }
