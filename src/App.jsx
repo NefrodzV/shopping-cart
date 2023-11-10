9b69a291a00b1dc59165532e096717c49d50dd84
Neftaly <neftalyrv23@gmail.com>
2023-11-10 11:03:27 -0400

    app gives the router and removed unused imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
 import React from 'react'
 import './App.css'
-import Header from './components/Header'
-import Specials from './components/Specials'
 import Router from './Router'
 
 export default function App() {
