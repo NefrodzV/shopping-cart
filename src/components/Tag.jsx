d8651e4d27718b9abadd08a60bc2b452eedbf2ec
Neftaly <neftalyrv23@gmail.com>
2023-11-11 12:27:44 -0400

    style tag component

--- src/components/Tag.jsx
+++ src/components/Tag.jsx
@@ -1,5 +1,13 @@
 import React from 'react'
 export default function Tag({ discount, show }) {
   // Will be displayed in items that have are on discount
-  return <div className={`tag${show ? ' show' : ''}`}>{discount}</div>
+  return (
+    <div aria-label="discount" className={`tag${show ? ' show' : ''}`}>
+      {discount}
+      <div>
+        <div className="tag-percent">%</div>
+        <div className="tag-off-text">off</div>
+      </div>
+    </div>
+  )
 }
