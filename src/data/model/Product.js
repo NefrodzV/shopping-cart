972bbcf59806c04f5e197f3b9601c8ec2581d555
Neftaly <neftalyrv23@gmail.com>
2023-11-11 10:55:18 -0400

    Added more brand and discount property

--- src/data/model/Product.js
+++ src/data/model/Product.js
@@ -4,16 +4,30 @@ export default class Product {
    * @param {number} id Identifier of the product
    * @param {string} title Name of the product
    * @param {number} price Price of the product
+   * @param {number} discount Percent to discount
+   * @param {string} brand Brand of the product
    * @param {string} description Brief description of the product
    * @param {string} category What category it belongs to
    * @param {string} url Image url
    * @param {number} rating Rating given by users
    */
-  constructor(id, title, price, description, category, url, rating) {
+  constructor(
+    id,
+    title,
+    price,
+    discount,
+    description,
+    brand,
+    category,
+    url,
+    rating
+  ) {
     this.id = id
     this.title = title
+    this.discount = discount
     this.price = price
     this.description = description
+    this.brand = brand
     this.category = category
     this.url = url
     this.rating = rating
@@ -31,10 +45,17 @@ export default class Product {
     return this.price
   }
 
+  get getDiscount() {
+    return this.discount
+  }
+
   get getDescription() {
     return this.description
   }
 
+  get getBrand() {
+    return this.brand
+  }
   get getCategory() {
     return this.category
   }
