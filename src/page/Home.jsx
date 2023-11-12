43f607065a53a15cac9aca0b17809ca5d46b10cc
Neftaly <neftalyrv23@gmail.com>
2023-11-12 15:18:28 -0400

    deleted component and fixed import

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -3,15 +3,8 @@ import Rating from '../component/Rating'
 import Tag from '../component/Tag'
 import CountdownTimer from '../component/CoundownTimer'
 import ProductoModal from '../component/ProductModal'
+import DiscountSection from '../component/DiscountSection'
 
 export default function Home() {
-  const [rating, setRating] = useState(4.5)
-  return (
-    <>
-      Home
-      <Rating rating={rating} />
-      <Tag discount={'5'} show={false} />
-      <CountdownTimer />
-    </>
-  )
+  return <>Home</>
 }
