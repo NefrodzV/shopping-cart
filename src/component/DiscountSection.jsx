319a5eb367a252fb299c329e75cfae16b556440a
Neftaly <neftalyrv23@gmail.com>
2023-11-12 14:05:15 -0400

    Section test complete

--- src/component/DiscountSection.jsx
+++ src/component/DiscountSection.jsx
@@ -1,1 +1,23 @@
-export default function DiscountSection() {}
+import CoundownTimer from './CoundownTimer'
+import Tag from './Tag'
+
+export default function DiscountSection({ products }) {
+  const createDiscountProduct = (product) => {
+    return (
+      <li key={product.getId}>
+        <img src={product.getUrl} alt={product.getDescription} />
+        <Tag discount={product.getDiscount} show={true} />
+      </li>
+    )
+  }
+  return (
+    <section>
+      <h2>
+        All product about {products[0].getCategory} are on discount for limited
+        time
+      </h2>
+      <CoundownTimer />
+      <ul>{products.map((product) => createDiscountProduct(product))}</ul>
+    </section>
+  )
+}
