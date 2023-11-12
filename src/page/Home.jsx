1a4048876ca63b83f55ef0c9052310971c5eaea0
Neftaly <neftalyrv23@gmail.com>
2023-11-12 19:03:53 -0400

    Process all data to the home page

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -1,10 +1,34 @@
-import { useState } from 'react'
-import Rating from '../component/Rating'
-import Tag from '../component/Tag'
-import CountdownTimer from '../component/CoundownTimer'
-import ProductoModal from '../component/ProductModal'
+import { useEffect, useState } from 'react'
 import DiscountSection from '../component/DiscountSection'
+import { useLoaderData } from 'react-router-dom'
+import Product from '../data/model/Product'
+import Util from '../Util'
 
 export default function Home() {
-  return <>Home</>
+  const data = useLoaderData()
+  const [productsState, setProducts] = useState([])
+
+  useEffect(() => {
+    // This will make it run only when the array is first empty
+    if (productsState.length === 0) {
+      const copy = [...productsState]
+      data.products.forEach((object) => {
+        const product = new Product(
+          object.id,
+          object.title,
+          object.price,
+          object.discount,
+          object.description,
+          object.brand,
+          object.category,
+          object.images[0],
+          object.rating
+        )
+        copy.push(product)
+      })
+      setProducts(copy)
+    }
+  }, [])
+
+  return <DiscountSection products={productsState} />
 }
