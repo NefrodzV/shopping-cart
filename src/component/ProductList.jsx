abe30be110a4c7042a9a7f43c88be824afbca814
Neftaly <neftalyrv23@gmail.com>
2023-11-14 18:04:18 -0400

    made the listener pass product as argument

--- src/component/ProductList.jsx
+++ src/component/ProductList.jsx
@@ -3,16 +3,19 @@ import Product from './Product'
 
 // Represents the item list
 export default function ProductList({ products, productListener }) {
-  console.log(products)
   const createProduct = (product) => {
     return (
-      <Product key={product.id} product={product} listener={productListener} />
+      <Product
+        key={product.id}
+        product={product}
+        listener={productListener.bind(this, product)}
+      />
     )
   }
 
   return (
-      <ul className="product-list">
-        {products.map((product) => createProduct(product))}
-      </ul>
+    <ul className="product-list">
+      {products.map((product) => createProduct(product))}
+    </ul>
   )
 }
