1516b56dd0338e259b7b1a0db97ac2813869f950
Neftaly <neftalyrv23@gmail.com>
2023-11-15 14:54:18 -0400

    style the modal and added logic for total

--- src/component/ProductModal.jsx
+++ src/component/ProductModal.jsx
@@ -2,16 +2,23 @@ import { useEffect, useRef, useState } from 'react'
 
 export default function ProductModal({ product, show, closeListener }) {
   const dialogRef = useRef(null)
-  const [total, setTotal] = useState('0.0')
+  const [total, setTotal] = useState()
+  const [quantity, setQuantity] = useState(1)
   /**@todo Make the listeners fucntionality */
 
-  const onSelected = () => {
-    console.log('onSelected called')
+  const onSelected = (e) => {
+    const value = e.target.value
+    setQuantity(value)
+    console.log('On selected called')
   }
 
   const submit = () => {
     console.log('on submit called')
   }
+
+  useEffect(() => {
+    setTotal(quantity * product.price)
+  }, [product, quantity])
 
   useEffect(() => {
     show ? dialogRef.current.showModal() : dialogRef.current.close()
@@ -23,18 +30,21 @@ export default function ProductModal({ product, show, closeListener }) {
         <header className="modal-header">
           <h3>{product.title}</h3>
           <button type="button" onClick={closeListener}>
-            Close
+            x
           </button>
         </header>
         <div className="modal-content">
-          <img src={product.url} alt={product.description} />
-          <p>{product.brand}</p>
-          <p>{product.description}</p>
-          <p>{product.price}</p>
+          <img
+            className="modal-img big-border"
+            src={product.url}
+            alt={product.description}
+          />
+          <p className="modal-brand">{product.brand}</p>
+          <p className="modal-description">{product.description}</p>
+          <p className="modal-price">&#x24; {product.price}</p>
           <label htmlFor="quatity-select">
-            Enter the quantity you want to buy:
+            Quantity:
             <select id="quantity-select" onClick={onSelected}>
-              <option value={0}>0</option>
               <option value={1}>1</option>
               <option value={2}>2</option>
               <option value={3}>3</option>
@@ -42,7 +52,7 @@ export default function ProductModal({ product, show, closeListener }) {
               <option value={5}>5</option>
             </select>
           </label>
-          <p>{total}</p>
+          <p className="modal-total">&#x24; {total}</p>
           <button type="button" onClick={submit}>
             Add to cart
           </button>
