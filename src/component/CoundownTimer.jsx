b1022e6774445499487966b7dd0c64835f8b97bb
Neftaly <neftalyrv23@gmail.com>
2023-11-16 12:54:46 -0400

    Changed var declaration

--- src/component/CoundownTimer.jsx
+++ src/component/CoundownTimer.jsx
@@ -3,16 +3,14 @@ import { useEffect, useState } from 'react'
 export default function CoundownTimer() {
   /**@todo Add some accesibility to this**/
   /**@todo Make a array of dates for the timer to work*/
-  const [time, setTime] = useState('')
-  const now = new Date()
-  const futureDate = new Date('Nov 20,2023 00:00:00')
-  const countDown = futureDate.getTime() - now.getTime()
+  const [time, setTime] = useState('0d 0h 0m 0s')
+
   const MILLI_IN_A_SECOND = 1000
   const MILLI_IN_A_MINUTE = MILLI_IN_A_SECOND * 60
   const MILLI_IN_A_HOUR = MILLI_IN_A_MINUTE * 60
   const MILLI_IN_A_DAY = MILLI_IN_A_HOUR * 24
 
-  const formatTime = () => {
+  const formatTime = (countDown) => {
     const days = Math.floor(countDown / MILLI_IN_A_DAY)
     const hours = Math.floor((countDown / MILLI_IN_A_HOUR) % 24)
     const minutes = Math.floor((countDown / MILLI_IN_A_MINUTE) % 60)
@@ -22,8 +20,11 @@ export default function CoundownTimer() {
   }
 
   useEffect(() => {
+    const now = new Date()
+    const futureDate = new Date('Nov 20,2023 00:00:00')
+    const countDown = futureDate.getTime() - now.getTime()
     const id = setInterval(() => {
-      formatTime()
+      formatTime(countDown)
     }, 1000)
     return function clean() {
       clearInterval(id)
