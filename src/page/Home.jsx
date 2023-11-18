6e7c8f0d201a11bb267ab3ea2be9ecbbf3bf2a55
Neftaly <neftalyrv23@gmail.com>
2023-11-18 10:57:52 -0400

    change the logic

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -2,7 +2,6 @@ import { useEffect, useState } from 'react'
 import DiscountSection from '../component/DiscountSection'
 import { useLoaderData } from 'react-router-dom'
 import Product from '../data/model/Product'
-import Util from '../Util'
 
 export default function Home() {
   const data = useLoaderData()
@@ -10,25 +9,27 @@ export default function Home() {
 
   useEffect(() => {
     // This will make it run only when the array is first empty
-    if (productsState.length === 0) {
-      const copy = [...productsState]
-      data.products.forEach((object) => {
-        console.log(object.discountPercentage)
-        const product = new Product(
-          object.id,
-          object.title,
-          object.price,
-          object.discountPercentage,
-          object.description,
-          object.brand,
-          object.category,
-          object.images[0],
-          object.rating
-        )
-        copy.push(product)
-      })
-      setProducts(copy)
-    }
+    // if (productsState.length === 0) {
+    const products = []
+    data.products.forEach((object) => {
+      const product = new Product(
+        object.id,
+        object.title,
+        object.price,
+        object.discountPercentage,
+        object.description,
+        object.brand,
+        object.category,
+        object.images[0],
+        object.rating
+      )
+
+      products.push(product)
+    })
+
+    setProducts(products)
+
+    // }
   }, [])
 
   return <DiscountSection products={productsState} />
