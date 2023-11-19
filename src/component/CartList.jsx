365ffa1a1ef11f6b63d352b92d7d7d255d1e07ea
Neftaly <neftalyrv23@gmail.com>
2023-11-19 14:13:34 -0400

    Implemented cart list logic

--- src/component/CartList.jsx
+++ src/component/CartList.jsx
@@ -1,3 +1,16 @@
-export default function CartList() {
-  return <h1>My cart list</h1>
+import CartItem from './CartItem'
+export default function CartList({
+  items,
+  editItemListener,
+  deleteItemListener
+}) {
+  const createItem = (item) => (
+    <CartItem
+      key={item.id}
+      item={item}
+      editListener={editItemListener}
+      cancelListener={deleteItemListener}
+    />
+  )
+  return <ul className="cart-list">{items?.map((item) => createItem(item))}</ul>
 }
